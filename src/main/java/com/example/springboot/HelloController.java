ea5a51d8a3f66b2cccc4529542c5e7356ff42057
HarishDemoGit <harishdurai2@gmail.com>
2024-05-02 11:49:05 +0530

    test1

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/eks1")
 public class HelloController {
+
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@GetMapping("/user")
 	public String index() {
@@ -17,5 +22,11 @@ public class HelloController {
 	@PostMapping("/post")
 	public String index_01(String name) {
 		return "hi EKS";
+	}
+
+	@GetMapping("/call-eks2")
+	public String callEks2() {
+		String url = "http://eks2-service.default.svc.cluster.local:8086/eks2/user";
+		return restTemplate.getForObject(url, String.class);
 	}
 }
